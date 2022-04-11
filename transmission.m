3e8e8b3e1d2e3318026b2362b702048af7e231d2
Roropey <romain.peyremorte@yahoo.fr>
2022-04-12 00:01:18 +0200

    avancement part5

--- transmission.m
+++ transmission.m
@@ -3,16 +3,23 @@ function [information_entree information_sortie x y z] = transmission(Fe,Rb,N,a,
 information_entree = randi([0,1], 1, nb_bits);
 
 % Calculs
-mapping = information_entree.*(a(2) - a(1)) + a(1);
-Suite_diracs = kron(mapping, [1 zeros(1, Fe/Rb-1)]);
-x = filter(h, 1, Suite_diracs);
+if (length(a)==2)
+    mapping = information_entree.*(a(2) - a(1)) + a(1);
+    Ns = Fe/Rb;
+elseif (length(a)==4)
+    information_entree_reshape = reshape(information_entree, [2 nb_bits/2]);
+    mapping = information_entree_reshape(1,:).*information_entree_reshape(2,:).*(a(4)-a(3)-a(1)+a(2))+ information_entree_reshape(1,:).*(a(3)-a(2)) + information_entree_reshape(2,:).*(a(1)-a(2)) + a(2); 
+    Ns = 2*Fe/Rb;
+end;
+Suite_diracs = kron(mapping, [1 zeros(1, Ns-1)])
+x = filter(h, 1, Suite_diracs)
 
 %% Canal de Transmission
 if (E_bN0Db < 0)
-    x_bruite = x;
+    x_bruite = x
 else
     P_x = mean(abs(x).^2);
-    Sigma_n = sqrt((P_x*Fe/Rb)/(2*log2(length(a))*10.^(E_bN0Db/10)));
+    Sigma_n = sqrt((P_x*Ns)/(2*log2(length(a))*10.^(E_bN0Db/10)));
     bruit = Sigma_n*randn(1, length(x));
     x_bruite = x + bruit;
 end;
@@ -23,8 +30,12 @@ z = filter(hr, 1, x_bruite);
 
 %Echantillonage
 
-z_echant = z(n0:Fe/Rb:end);
-information_sortie = z_echant > 0;   
-
+z_echant = z(n0:Ns:end);
+if (length(a)==2)
+    information_sortie = z_echant > (a(1)+a(2))/2;
+elseif (length(a)==4)
+    information_sortie_reshape = [z_echant>(a(2)+a(3))/2;abs(z_echant)>8*(a(3)+a(4))/2];
+    information_sortie = reshape(information_sortie_reshape,1,nb_bits);
+end;
 y=0;
 end
