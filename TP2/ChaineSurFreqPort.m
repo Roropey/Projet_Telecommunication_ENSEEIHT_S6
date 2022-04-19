d35e8d40174ad58f6389bcc612ef865d943d9d45
Rossi Pierre <prossi2@brochet.enseeiht.fr>
2022-04-19 12:01:00 +0200

    fin séance du 19/04

--- TP2/ChaineSurFreqPort.m
+++ TP2/ChaineSurFreqPort.m
@@ -53,17 +53,6 @@ x = real(xe .* exp(2*1i*pi*Fp*t));
 DSP = fftshift(abs(fft(xcorr(x,'unbiased'),10000)));
 plage=(-Fe/2 : Fe/2 - 1) * Fe/(length(DSP)-1);
 
-% Retour en bande de base
-xcos = x.*cos(2*pi*Fp*t);
-xsin = x.*sin(2*pi*Fp*t);
-    % Faire un filtrage passe-bas
-    % Multiplier xsin par -j et faire la somme
-
-% Démodulation
-
-%syms expr_th_1(f);
-%expr_th_1(f) = var(mapping_1)*(Ns_1/Fe).*(sinc(f *(Ns_1/Fe))).^2;
-
 % Affichage
 figure('Name', 'Signal modulé')
 
@@ -87,3 +76,74 @@ ylabel('Amplitude');
 
 subplot(2,2,4);
 semilogy(plage, DSP);
+
+% Retour en bande de base
+xcos = x.*cos(2*pi*Fp*t);
+xsin = x.*sin(2*pi*Fp*t);
+
+% Filtrage passe-bas
+Fc = 3500;
+Plagef = ((-50:50)'*(1/Fe));
+Filt_PB = 2*Fc*sinc(pi*2*Fc*Plagef);
+%xcox_filt = filter(Filt_PB, 1, xcos);
+xcos_filt = filter(sinc(Fc*Plagef),1,xcos);
+%xsin_filt = filter(Filt_PB, 1, xsin);
+xsin_filt = filter(sinc(Fc*Plagef),1,xsin);
+
+% Affichage
+figure('Name', "Filtrage Passe-Bas");
+subplot(2,2,1);
+plot(xcos_filt);
+title("Xcos filtré, en temporel");
+xlabel("Temps");
+ylabel("Amplitude");
+subplot(2,2,3);
+plot(xsin_filt);
+title("Xsin filtré, en temporel");
+xlabel("Temps");
+ylabel("Amplitude");
+
+DSP_cos = fftshift(abs(fft(xcorr(xcos_filt,'unbiased'),10000)));
+subplot(2,2,2);
+semilogy(plage, DSP_cos);
+title("Xcos filtré, en fréquentiel");
+DSP_sin = fftshift(abs(fft(xcorr(xsin_filt,'unbiased'),10000)));
+subplot(2,2,4);
+semilogy(plage, DSP_sin);
+title("Xsin filtré, en fréquentiel");
+
+xsin_filt = xsin_filt*1i;
+
+x_demod = xcos_filt - xsin_filt;
+
+% Démodulation
+hr = rcosdesign(alpha, (N-1)/Ns,Ns);
+z = filter(hr, 1, x_demod);
+
+n0 = 8;
+z_echant = z(n0:Ns:end);
+z_reel_recu = real(z_echant) > 0;
+z_imag_recu = imag(z_echant) > 0;
+z_recu = [z_reel_recu; z_imag_recu];
+z_recu_reshape = reshape(z_recu, 1, nb_bits);
+
+TEB = [];
+E_bN0dB = 0:0.5:8;
+
+for k=E_bN0dB_2
+    nb_bits_faux = 0;
+    nb_bits_tot = 0;
+    while nb_bits_faux < seuil_erreur
+        % Mettre les calculs du dessus
+        [info_binaire_env, info_binaire_recu, ~, ~, x, ~, z] = transmission(Fe,Rb,N,a,nb_bits,k,n0,h,0,hr);
+        nb_bits_faux = sum(abs(info_binaire_recu-info_binaire_env)) + nb_bits_faux;
+        nb_bits_tot = nb_bits_tot + nb_bits;
+    end;
+    TEB = [TEB nb_bits_faux/nb_bits_tot];
+end;
+
+%syms expr_th_1(f);
+%expr_th_1(f) = var(mapping_1)*(Ns_1/Fe).*(sinc(f *(Ns_1/Fe))).^2;
+
+
+
