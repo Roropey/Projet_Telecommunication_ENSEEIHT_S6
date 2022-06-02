d871bde933c3827a5de5b8d5d0f7a85829b3b01e
Roropey <romain.peyremorte@yahoo.fr>
2022-06-02 21:42:06 +0200

    correction affichage const

--- TP2/ChaineSurFreqPortEq.m
+++ TP2/ChaineSurFreqPortEq.m
@@ -134,13 +134,50 @@ fprintf("Taux d'erreur sans bruit pour n0 = %.1f : %.4f.\n", n0, taux_erreur_bin
 
 %% Avec bruit
 
+% Constellations
+E_bN0db_Cons = 0:1:8;
+for E_bN0 = E_bN0db_Cons
+    info_binaire = randi([0,1], 1,nb_bits);
+    % Modulation sur fréquence porteuse
+    info_binaire_2 = reshape(info_binaire, [2 nb_bits/2]);
+    mapping = (info_binaire_2(1, :).* (a_11 - a_01) + a_01) + 1i*(info_binaire_2(2, :).* (b_11 - b_10) + b_10);
+    s1 = scatterplot(mapping);
+    s1.Name=strcat("Constellation sortie mapping pour bruit de ",int2str(E_bN0)," dB");
+    Suite_diracs = kron(mapping, [1 zeros(1, Ns-1)]);
+    Suite_diracs_decale=[Suite_diracs zeros(1,floor(N/2))]; 
+    xe_decale = filter(h, 1, Suite_diracs_decale);
+    xe = xe_decale(floor(N/2)+1:end);
+    
+   
+    % Ajout du bruit
+    P_re =  mean(abs(xe).^2);
+    Sigma_n = sqrt((P_re*2*Fe/Rb)/(2*log2(M)*10.^(E_bN0/10)));
+    bruit = Sigma_n*randn(1, length(x))+1i*Sigma_n*randn(1, length(x));
+    x_bruite = xe + bruit; 
 
+    
+    % Démodulation
+    hr = h; %rcosdesign(alpha, (N-1)/Ns,Ns);
+    x_demod_decale = [x_bruite zeros(1,floor(N/2))];
+    z_decale = filter(hr, 1, x_demod_decale);
+    z = z_decale(floor(N/2)+1:end);%
+    
+    n0 = 1;
+    z_echant = z(n0:Ns:end);
+    s2=scatterplot(z_echant);
+    s2.Name = strcat("Constellation sortie échantillonneyr pour bruit de ",int2str(E_bN0)," dB");
+    z_fort = real(z_echant) > 0;
+    z_faible = imag(z_echant) < 0;
+    z_recu = [z_fort; z_faible];
+    z_recu_reshape = reshape(z_recu, 1, nb_bits);
+end;
 
+% TEB
 TEB = [];
-E_bN0db = 0:0.1:6;
+E_bN0db_TEB = 0:0.1:6;
 
 
-for E_bN0 = E_bN0db
+for E_bN0 = E_bN0db_TEB
     nb_bits_faux = 0;
     nb_bits_tot = 0;
     while nb_bits_faux < seuil_erreur
@@ -154,22 +191,16 @@ for E_bN0 = E_bN0db
         xe = xe_decale(floor(N/2)+1:end);
         
        
-
+        % Ajout du bruit
         P_re =  mean(abs(xe).^2);
         Sigma_n = sqrt((P_re*2*Fe/Rb)/(2*log2(M)*10.^(E_bN0/10)));
         bruit = Sigma_n*randn(1, length(x))+1i*Sigma_n*randn(1, length(x));
-        x_bruite = xe + bruit;
-        %figure();subplot(2,2,1);plot(x);subplot(2,2,2);plot(bruit);subplot(2,2,[3 4]);p1=plot(x);hold on;p2=plot(bruit);hold off;legend([p1,p2],"x","bruit");
+        x_bruite = xe + bruit; 
 
-        
-        
-        
-        
-        x_demod = x_bruite;
         
         % Démodulation
         hr = h; %rcosdesign(alpha, (N-1)/Ns,Ns);
-        x_demod_decale = [x_demod zeros(1,floor(N/2))];
+        x_demod_decale = [x_bruite zeros(1,floor(N/2))];
         z_decale = filter(hr, 1, x_demod_decale);
         z = z_decale(floor(N/2)+1:end);%
         
@@ -189,13 +220,13 @@ for E_bN0 = E_bN0db
 end;
 
 
-TEB_th = (4/ log2(M)).*(1-(1/sqrt(M))).*qfunc(sqrt(((3*log2(M))/(M-1)).*10.^(E_bN0db/10)));
+TEB_th = (4/ log2(M)).*(1-(1/sqrt(M))).*qfunc(sqrt(((3*log2(M))/(M-1)).*10.^(E_bN0db_TEB/10)));
 
 figure('Name', "Taux Erreur Binaire",'Position', [100 100 1300 600]);
-s1_TEB = semilogy(E_bN0db,TEB);
+s1_TEB = semilogy(E_bN0db_TEB,TEB);
 hold on;
 
-s2_TEB = semilogy(E_bN0db,TEB_th);
+s2_TEB = semilogy(E_bN0db_TEB,TEB_th);
 
 hold off;
 xlabel('Eb/N0 (dB)');
